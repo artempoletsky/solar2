ef093ac545d9fa1751c762d6db88dcf20623979b
Artem Poletsky <artempoletsky@gmail.com>
2016-12-02 20:58:59 +0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,158 +1,43 @@
-$(function () {
+var App = {
+    /**
+     * template for table cell div
+     */
+    $cellTempl: undefined,
+    /**
+     * main table object
+     */
+    $table: undefined,
+    /**
+     * flag when content editable is active
+     */
+    recordingFormula: false,
+    /**
+     * position of current editable cell {x: 0, y: 0}
+     */
+    recordingPosition: undefined,
+    /**
+     * string representation of cell
+     */
+    variableExp: /([A-Z]+)(\d+)/ig,
+    /**
+     * App entry point
+     */
+    ready: function () {
+        App.$cellTempl = $('.table_cell');
+        App.$table = $('.main_table');
+        App.drawTable(10, 10);
 
-    var $cellTempl = $('.table_cell');
+        $('body').on('keydown', App.onKeyDown);
 
-    var $table = $('.main_table');
-
-    function drawTable(width, height) {
-        $table.empty();
-        for (var i = 0; i < height; i++) {
-            var $tr = $.make('tr');
-            for (var j = 0; j < width; j++) {
-                var $td = $.make('td');
-                $tr.append($td.append($cellTempl.clone()));
-            }
-            $table.append($tr);
-        }
-    }
-
-    drawTable(10, 10);
-
-
-    function toLetters(num) {
-        var mod = num % 26;
-        var pow = num / 26 | 0;
-        var out = mod ? String.fromCharCode(64 + mod) : (pow--, 'Z');
-        return pow ? toLetters(pow) + out : out;
-    }
-
-    function fromLetters(str) {
-        var out = 0,
-            len = str.length,
-            pos = len;
-        while ((pos -= 1) > -1) {
-            out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - 1 - pos);
-        }
-        return out;
-    }
-
-    $('body').on('keydown', function (e) {
-        //console.log(e);
-        if (recordingFormula && e.keyCode == 13) {
-            e.preventDefault();
-
-            var pos = getPosition($('.editable'));
-            stopRecordingFormula();
-            setFocus(pos.x, pos.y + 1);
-        }
-    });
-
-    function getCellByPosition(x, y) {
-        return $table.children().eq(y).children().eq(x).children().eq(0);
-    };
-
-    function setFocus(x, y) {
-        var $cell;
-        if (y) {
-            $cell = getCellByPosition(x, y);
-        } else {
-            $cell = $(x);
-        }
-        $('.table_cell.focused').removeClass('focused');
-        $cell.addClass('focused');
-    };
-
-    var variableExp = /([A-Z]+)(\d+)/ig;
-
-    //name = AA11 etc
-    function getCellByName(name) {
-        name = name.toUpperCase();
-        variableExp.lastIndex = 0;
-        var res = variableExp.exec(name);
-
-        var x = fromLetters(res[1]) - 1;
-        var y = res[2] - 1;
-
-        return getCellByPosition(x, y);
-    };
-
-
-
-    function getCellValue($cell) {
-        var formula = $cell.data('formula');
-
-        if (!formula) {
-            return 0;
-        }
-
-        variableExp.lastIndex = 0;
-        formula = formula.replace(variableExp, function (cell) {
-            return getCellValue(getCellByName(cell));
-        });
-
-        formula = $.trim(formula, '=');
-
-
-        return eval(formula);
-    };
-
-
-    function stopRecordingFormula() {
-        var $editable = $('.editable')
-            .removeAttr('contenteditable')
-            .removeClass('editable');
-        var formula = $editable.html();
-        $editable.data('formula', formula);
-
-        $editable.html(getCellValue($editable));
-        Selection.clear();
-        recordingFormula = false;
-    }
-
-    function getPosition($cell) {
-        var $parent = $cell.parent();
-        return {
-            x: $parent.index(),
-            y: $parent.parent().index()
-        };
-    };
-
-
-    var recordingFormula = false;
-    var recordingPosition;
-    $table.on('click', function (e) {
-        if (e.target.className == 'table_cell') {
-            var $target = $(e.target);
-
-
-            if (recordingFormula) {
-
-                var $editable = $('.editable');
-                var formula = $editable.html();
-                if (formula.startsWith('=')) {
-                    var pos = getPosition($target);
-
-                    Selection.insertTextAtCursor(toLetters(pos.x + 1) + (pos.y + 1));
-                } else {
-                    stopRecordingFormula();
-                }
-                //console.log();
-            } else {
-                setFocus($target);
-            }
-        }
-    });
-
-    $table.on('mousedown', function (e) {
-        if (!recordingFormula) {
-            e.preventDefault() // если ничего не записываем, то бывает лишнее выделение появляется, это его отключает.
-        } else if (!e.target.classList.contains('editable')) {
-            e.preventDefault(); //Если записываем надо, чтобы курсор устанавливался в активное поле ввода, и не устанавливался в другие
-        }
-    });
-
-    $table.on('dblclick', function (e) {
-        //console.log(e.target);
+        App.$table.on('click', App.onTableClick);
+        App.$table.on('mousedown', App.onTableMouseDown);
+        App.$table.on('dblclick', App.startRecordingFormula);
+    },
+    /**
+     *
+     * @param e - event object
+     */
+    startRecordingFormula: function (e) {
         if (e.target.classList.contains('table_cell')) {
 
             var $target = $(e.target);
@@ -162,8 +47,155 @@ $(function () {
                 .addClass('editable')
                 .focus();
 
-            recordingPosition = getPosition($target);
-            recordingFormula = true;
+            App.recordingPosition = App.getPosition($target);
+            App.recordingFormula = true;
         }
-    });
-});
+    },
+    drawTable: function (width, height) {
+        this.$table.empty();
+        for (var i = 0; i < height; i++) {
+            var $tr = $.make('tr');
+            for (var j = 0; j < width; j++) {
+                var $td = $.make('td');
+                $tr.append($td.append(this.$cellTempl.clone()));
+            }
+            this.$table.append($tr);
+        }
+    },
+    toLetters: function (num) {
+        var mod = num % 26;
+        var pow = num / 26 | 0;
+        var out = mod ? String.fromCharCode(64 + mod) : (pow--, 'Z');
+        return pow ? this.toLetters(pow) + out : out;
+    },
+    fromLetters: function (str) {
+        var out = 0,
+            len = str.length,
+            pos = len;
+        while ((pos -= 1) > -1) {
+            out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - 1 - pos);
+        }
+        return out;
+    },
+    onKeyDown: function (e) {
+        if (App.recordingFormula && e.keyCode == 13) {
+            e.preventDefault();
+
+            var pos = App.getPosition($('.editable'));
+            App.stopRecordingFormula();
+            App.setFocus(pos.x, pos.y + 1);
+        }
+    },
+    stopRecordingFormula: function () {
+        var $editable = $('.editable')
+            .removeAttr('contenteditable')
+            .removeClass('editable');
+        var formula = $editable.html();
+        $editable.data('formula', formula);
+
+        $editable.html(App.getCellValue($editable));
+        Selection.clear();
+        App.recordingFormula = false;
+    },
+    setFocus: function (x, y) {
+        var $cell;
+        if (y) {
+            $cell = this.getCellByPosition(x, y);
+        } else {
+            $cell = $(x);
+        }
+        $('.table_cell.focused').removeClass('focused');
+        $cell.addClass('focused');
+    },
+    getCellByPosition: function (x, y) {
+        return this.$table.children().eq(y).children().eq(x).children().eq(0);
+    },
+    /**
+     * @param name AA11 etc
+     */
+    getCellByName: function (name) {
+        name = name.toUpperCase();
+        App.variableExp.lastIndex = 0;
+        var res = App.variableExp.exec(name);
+
+        var x = App.fromLetters(res[1]) - 1;
+        var y = res[2] - 1;
+
+        return App.getCellByPosition(x, y);
+    },
+    getCellValue: function ($cell) {
+        var formula = $cell.data('formula');
+
+        if (!formula) {
+            return 0;
+        }
+
+        this.variableExp.lastIndex = 0;
+        formula = formula.replace(this.variableExp, function (cell) {
+            return App.getCellValue(this.getCellByName(cell));
+        });
+
+        formula = $.trim(formula, '=');
+
+
+        return eval(formula);
+    },
+
+    getCellValue: function ($cell) {
+        var formula = $cell.data('formula');
+
+        if (!formula) {
+            return 0;
+        }
+
+        this.variableExp.lastIndex = 0;
+        formula = formula.replace(this.variableExp, function (cell) {
+            return App.getCellValue(App.getCellByName(cell));
+        });
+
+        formula = $.trim(formula, '=');
+
+
+        return eval(formula);
+    },
+
+    getPosition: function ($cell) {
+        var $parent = $cell.parent();
+        return {
+            x: $parent.index(),
+            y: $parent.parent().index()
+        };
+    },
+
+    onTableClick: function (e) {
+        if (e.target.className == 'table_cell') {
+            var $target = $(e.target);
+
+
+            if (App.recordingFormula) {
+
+                var $editable = $('.editable');
+                var formula = $editable.html();
+                if (formula.startsWith('=')) {
+                    var pos = App.getPosition($target);
+
+                    Selection.insertTextAtCursor(App.toLetters(pos.x + 1) + (pos.y + 1));
+                } else {
+                    App.stopRecordingFormula();
+                }
+                //console.log();
+            } else {
+                App.setFocus($target);
+            }
+        }
+    },
+    onTableMouseDown: function (e) {
+        if (!App.recordingFormula) {
+            e.preventDefault() // если ничего не записываем, то бывает лишнее выделение появляется, это его отключает.
+        } else if (!e.target.classList.contains('editable')) {
+            e.preventDefault(); //Если записываем надо, чтобы курсор устанавливался в активное поле ввода, и не устанавливался в другие
+        }
+    }
+};
+
+$(App.ready);
